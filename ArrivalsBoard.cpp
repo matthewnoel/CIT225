5dd8b4fa58df401e5ca566b09da9fa6ed6d369b8
Matthew Noel <31493274+matthewnoel@users.noreply.github.com>
2018-03-21 08:48:09 -0500

    Arrivals Stuff
    Pseudo code

--- ArrivalsBoard.cpp
+++ ArrivalsBoard.cpp
@@ -13,33 +13,42 @@ void printFormattedTime(long);
 char displayMenu();
 
 int main() {
+        
         char option;
 
         do {
                 option = displayMenu();
                 switch (option) {
-                case   'G':
-                case   'g':
+                case 'A':
+                case 'a':
 
                         break;
-                case 'H':
-                case 'h':
+                case 'C':
+                case 'c':
 
                         break;
-                case 'R':
-                case 'r':
+                case 'D':
+                case 'd':
 
                         break;
+
+                case 'M':
+                case 'm':
+
+                        break;
+
                 case 'Q':
                 case 'q':
 
                         break;
                 default:
                         cout << "Please select a valid option" << endl;
+                }
 
         } while (option != 'q' && option != 'Q');
 
         system("pause");
+        
         return 0;
 }
 
@@ -60,4 +69,48 @@ char displayMenu() {
         cout << "(Q)uit" << endl;
         cin >> c;
         return c;
+}
+
+//Pseudo code
+SelectionSort(numbers, numbersSize) {
+i = 0
+j = 0
+indexSmallest = 0
+temp = 0  // Temporary variable for swap
+
+for (i = 0; i < numbersSize - 1; ++i) {
+
+// Find index of smallest remaining element
+indexSmallest = i
+for (j = i + 1; j < numbersSize; ++j) {
+
+if (numbers[j] < numbers[indexSmallest]) {
+indexSmallest = j
+}
+}
+
+// Swap numbers[i] and numbers[indexSmallest]
+temp = numbers[i]
+numbers[i] = numbers[indexSmallest]
+numbers[indexSmallest] = temp
+}
+}
+
+// Pseudo Code
+void binarySearch(numbers, numbersSize, key) {
+        mid = 0
+        low = 0
+        high = numbersSize - 1
+
+        while (high >= low) {
+                mid = (high + low) / 2
+                if (numbers[mid] < key) {
+                        low = mid + 1
+                } else if (numbers[mid] > key) {
+                        high = mid - 1
+                } else {
+                        return mid
+                }
+        }
+        return -1
 }
