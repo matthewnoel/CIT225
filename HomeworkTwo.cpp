a89e4eb54aee507933d257b60261bc439f8b2145
Matthew Noel <31493274+matthewnoel@users.noreply.github.com>
2018-04-10 02:04:11 -0500

    Started Homework 2

---
+++ HomeworkTwo.cpp
@@ -0,0 +1,104 @@
+#include<cstdlib>
+
+using namespace std;
+
+void insertionSort(long[], long);
+void selectionSort(long[], long);
+void quickSort(long[], long, long);
+long partition(long[], long, long);
+void shellSort();
+
+int main() {
+
+        return 0;
+}
+
+// Smallest to largest
+void insertionSort(long numbers[], long numbersSize) {
+        int i = 0;
+        int j = 0;
+        int temp = 0;
+
+        for (int i = 1; i < numbersSize; i++) {
+                j = i;
+                while (j > 0 && numbers[j] < numbers[j - 1]) {
+                        temp = numbers[j];
+                        numbers[j] = numbers[j - 1];
+                        numbers[j - 1] = temp;
+                        j--;
+                }
+        }
+}
+
+// Smallest to largest
+void selectionSort(long numbers[], long numbersSize) {
+        int i = 0;
+        int j = 0;
+        int indexSmallest = 0;
+        int temp = 0;
+
+        for (int i = 0; i < numbersSize - 1; i++) {
+                indexSmallest = i;
+                for (int j = i + 1; j < numbersSize; j++) {
+                        if (numbers[j] < numbers[indexSmallest]) {
+                                indexSmallest = j;
+                        }
+                }
+
+                temp = numbers[i];
+                numbers[i] = numbers[indexSmallest];
+                numbers[indexSmallest] = temp;
+        }
+}
+
+// Smallest to largest
+void quickSort(long numbers[], long i, long k) {
+        int j = 0;
+
+        if (i >= k) {
+                return;
+        }
+
+        j = partition(numbers, i, k);
+
+        quickSort(numbers, i, j);
+        quickSort(numbers, j + 1, k);
+}
+
+long partition(long numbers[], long i, long k) {
+        int l = 0;
+        int h = 0;
+        int midpoint = 0;
+        int pivot = 0;
+        int temp = 0;
+        bool done = false;
+
+        midpoint = i + (k - i) / 2;
+        pivot = numbers[midpoint];
+
+        l = i;
+        h = k;
+
+        while (!done) {
+                while (numbers[l] < pivot) {
+                        l++;
+                }
+
+                while (pivot < numbers[h]) {
+                        h--;
+                }
+
+                if (l >= h) {
+                        done = true;
+                } else {
+                        temp = numbers[l];
+                        numbers[l] = numbers[h];
+                        numbers[h] = temp;
+
+                        l++;
+                        h--;
+                }
+        }
+
+        return h;
+}
